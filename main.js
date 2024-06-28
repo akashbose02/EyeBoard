7d4dfd1bc902169d6995968b16cdb1ab04dae7e5
AKASH BOSE <akashbose2002@gmail.com>
2024-06-29 00:48:28 +0530

    revert back changes vague

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 80,
+    width: 800,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
